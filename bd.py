8cc750fb8702356470eed7e6607e175692328c67
KapitanNemondo <mai.elovskij.dima@gmail.com>
2022-07-26 16:22:48 +0300

    version 0.1.2

--- bd.py
+++ bd.py
@@ -11,7 +11,6 @@ price = 200
 
 #cur.execute("CREATE TABLE IF NOT EXISTS `test` (`ID` INT, `NFTcount` INT, `Score` INT)")
 
-
 def Connect():     #Подключение к базе-данных
     global connection
     try:
@@ -30,44 +29,56 @@ def Connect():     #Подключение к базе-данных
 
 
 def NewUserNFT(id):  # Добавление нового пользователя
-    Connect()
-    with connection.cursor() as cursor:
-        try:
-            cursor.execute(f"INSERT INTO `base_user` VALUES ('{id}', '{''}')")
-        except:
-            pass
-        connection.commit()
-    connection.close()
+    
+    try:
+        with connection.cursor() as cursor:
+            try:
+                cursor.execute(f"INSERT INTO `base_user` VALUES ('{id}', '{''}')")
+            except:
+                pass
+            connection.commit()
+    except:
+        Connect()
+        NewUserNFT(id)
+    
 
 def ToWriteNumberScore(id, adress): # Запись номера тон счета с привязкой к id
-    Connect()
-    with connection.cursor() as cursor:
-        cursor.execute(f"UPDATE base_user SET ton_number = '{adress}' WHERE telegramm_id= '{id}'")
-        connection.commit()
-    connection.close()
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(f"UPDATE base_user SET ton_number = '{adress}' WHERE telegramm_id= '{id}'")
+            connection.commit()
+    except:
+        Connect()
+        NewUserNFT(id)
+    
 
 def GetReadNumberScore(id):         # Получение номера тон счета
-    Connect()
-    with connection.cursor() as cursor:
-        try:
-            cursor.execute(f"SELECT ton_number FROM `base_user` WHERE telegramm_id= '{id}'")
-            row = cursor.fetchone()
-            connection.close()
-            return row['ton_number']
-        except:
-            return "Вы не привязали ваш ton счет"
+    try:
+        with connection.cursor() as cursor:
+            try:
+                cursor.execute(f"SELECT ton_number FROM `base_user` WHERE telegramm_id= '{id}'")
+                row = cursor.fetchone()
+                connection.close()
+                return row['ton_number']
+            except:
+                return "Вы не привязали ваш ton счет"
+    except:
+        Connect()
+        GetReadNumberScore(id)
 
 def ChekNumberScore(id):            # Проверка наличия номер тон счета
-    Connect()
-    with connection.cursor() as cursor:
-        cursor.execute(f"SELECT ton_number FROM `base_user` WHERE telegramm_id= '{id}'")
-        row = cursor.fetchone()['ton_number']
-        
-        connection.close()
-        if row != '':
-            return True
-        else:
-            return False
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(f"SELECT ton_number FROM `base_user` WHERE telegramm_id= '{id}'")
+            row = cursor.fetchone()['ton_number']
+            
+            if row != '':
+                return True
+            else:
+                return False
+    except:
+        Connect()
+        ChekNumberScore(id)
 
 def ToWriteBdNFT(id, count_nft, score): # Проверка транзакции, в случае успеха запись данных в БД
     flag_transaktion = False
@@ -103,21 +114,22 @@ def ToWriteBdNFT(id, count_nft, score): # Проверка транзакции,
     return False
 
 def GetRandNFT(data, ton_number):  # Получение рандомного номера из базы данных и запись даты, времени, номера и id покупки
-    Connect()
-    with connection.cursor() as cursor:
-        try:
-            cursor.execute(f"SELECT nft_id FROM `base_nft` WHERE acsess= 'YES'")
-            row = cursor.fetchall()
-            id_nft = (row[random.randrange(len(row))]['nft_id'])
+    try:
+        with connection.cursor() as cursor:
+            try:
+                cursor.execute(f"SELECT nft_id FROM `base_nft` WHERE acsess= 'YES'")
+                row = cursor.fetchall()
+                id_nft = (row[random.randrange(len(row))]['nft_id'])
 
-            cursor.execute(f"UPDATE base_nft SET acsess = 'NO' WHERE nft_id= '{id_nft}'")
-            cursor.execute(f"INSERT INTO `shop_user` VALUES ('{str(data.date())}', '{str(data.time())}', '{ton_number}', '{id_nft}')")
-            connection.commit()
-            connection.close()
-            return id_nft
-        except:
-            connection.close()
-            return 6666
+                cursor.execute(f"UPDATE base_nft SET acsess = 'NO' WHERE nft_id= '{id_nft}'")
+                cursor.execute(f"INSERT INTO `shop_user` VALUES ('{str(data.date())}', '{str(data.time())}', '{ton_number}', '{id_nft}')")
+                connection.commit()
+                return id_nft
+            except:
+                return 6666
+    except:
+        Connect()
+        GetRandNFT(data, ton_number)
 
 def GetScore(id):   # Получение счета
     return "[Data Base] Eror 6666 - {Отказ в доступе}"  # DELETE AS CONFIGURATE NORM
@@ -126,3 +138,5 @@ def GetScore(id):   # Получение счета
         row = cursor.fetchone()
 
         return row['ton_number']
+
+Connect()
